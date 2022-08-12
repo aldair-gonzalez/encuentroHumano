5f276e2eae05e02cc3a242f0f6c4ed33f06c2453
Aldair Gonzalez <aldairgome97@gmail.com>
2022-08-12 13:28:21 -0500

    Añadiendo las tipografías (Open-sans, Segoe-Print) a la web en ./scss/root/variables.scss

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-'use strict';
+const header = document.getElementById('Header');
+
+/*
+// Carousel
+*/
+// const ToReturn = document.getElementById('To-return');
+// const ToNext = document.getElementById('To-next');
+// const CarouselItems = [...document.querySelectorAll('.Carousel-item')];
+
+
+// Margen a todo el body
+document.body.style.marginTop = header.offsetHeight + 'px';
+
+
+// let i = 0;
+
+// function handleClick() {
+//     i === CarouselItems.length ? i = 0 : null;
+//     i < 0 ? i = CarouselItems.length - 1 : null;
+
+//     CarouselItems.forEach(item => {
+//         item.style.transform = `translateX(-${i * 100}%)`;
+//     });
+// }
+// ToNext.addEventListener('click', () => {
+//     i++
+//     handleClick();
+// });
+// ToReturn.addEventListener('click', () => {
+//     i--;
+//     handleClick();
+// });
