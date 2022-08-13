6770b51eaa2097280b777c8614118038b645b0b6
Aldair Gonzalez <aldairgome97@gmail.com>
2022-08-13 16:35:27 -0500

    Carousel descktop

--- js/index.js
+++ js/index.js
@@ -1,32 +1,29 @@
 const header = document.getElementById('Header');
 
-/*
-// Carousel
-*/
-// const ToReturn = document.getElementById('To-return');
-// const ToNext = document.getElementById('To-next');
-// const CarouselItems = [...document.querySelectorAll('.Carousel-item')];
-
-
 // Margen a todo el body
 document.body.style.marginTop = header.offsetHeight + 'px';
 
+const carouselButtonBefore = document.getElementById('Carousel-button-before');
+const carouselButtonNext = document.getElementById('Carousel-button-next');
 
-// let i = 0;
+const carouselItems = [...document.getElementsByClassName('Carousel-item')];
 
-// function handleClick() {
-//     i === CarouselItems.length ? i = 0 : null;
-//     i < 0 ? i = CarouselItems.length - 1 : null;
+console.log(carouselItems);
 
-//     CarouselItems.forEach(item => {
-//         item.style.transform = `translateX(-${i * 100}%)`;
-//     });
-// }
-// ToNext.addEventListener('click', () => {
-//     i++
-//     handleClick();
-// });
-// ToReturn.addEventListener('click', () => {
-//     i--;
-//     handleClick();
-// });
+
+let i = 0;
+carouselItems[0].classList.add('is-active');
+function toggleActive() {
+    i >= carouselItems.length ? i = 0 : null;
+    i < 0 ? i = carouselItems.length - 1 : null;
+    carouselItems.forEach(item => item.classList.remove('is-active'));
+    carouselItems[i].classList.add('is-active');
+}
+carouselButtonNext.addEventListener('click', () => {
+    i++;
+    toggleActive();
+});
+carouselButtonBefore.addEventListener('click', () => {
+    i--;
+    toggleActive();
+});
