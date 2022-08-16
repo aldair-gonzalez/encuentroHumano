151ec813ee949646e29c3b236cb9876be396614d
Aldair Gonzalez <aldairgome97@gmail.com>
2022-08-16 17:40:19 -0500

    Seccion 'Que es' desktop

--- js/index.js
+++ js/index.js
@@ -8,22 +8,23 @@ const carouselButtonNext = document.getElementById('Carousel-button-next');
 
 const carouselItems = [...document.getElementsByClassName('Carousel-item')];
 
-console.log(carouselItems);
 
-
-let i = 0;
-carouselItems[0].classList.add('is-active');
-function toggleActive() {
-    i >= carouselItems.length ? i = 0 : null;
-    i < 0 ? i = carouselItems.length - 1 : null;
-    carouselItems.forEach(item => item.classList.remove('is-active'));
-    carouselItems[i].classList.add('is-active');
+// Carousel
+if (carouselItems.length > 0) {
+    let i = 0;
+    carouselItems[0].classList.add('is-active');
+    function toggleActive() {
+        i >= carouselItems.length ? i = 0 : null;
+        i < 0 ? i = carouselItems.length - 1 : null;
+        carouselItems.forEach(item => item.classList.remove('is-active'));
+        carouselItems[i].classList.add('is-active');
+    }
+    carouselButtonNext.addEventListener('click', () => {
+        i++;
+        toggleActive();
+    });
+    carouselButtonBefore.addEventListener('click', () => {
+        i--;
+        toggleActive();
+    });
 }
-carouselButtonNext.addEventListener('click', () => {
-    i++;
-    toggleActive();
-});
-carouselButtonBefore.addEventListener('click', () => {
-    i--;
-    toggleActive();
-});
