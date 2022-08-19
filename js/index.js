9f47ce9bc96954f05b6be97bc16979c630a05fb7
Aldair Gonzalez <aldairgome97@gmail.com>
2022-08-19 12:55:24 -0500

    Seccion Que es Responsive

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ const hamburger = document.getElementById('Hamburger');
 
 // Encuentros Slide Cards
 const encuentrosCards = document.getElementById('Encuentros-cards');
-const cards = [...encuentrosCards.children];
 const EncuentrosBtnAfter = document.getElementById('Encuentros-btn--after');
 const EncuentrosBtnBefore = document.getElementById('Encuentros-btn--before');
 
@@ -20,23 +19,25 @@ const EncuentrosBtnBefore = document.getElementById('Encuentros-btn--before');
 document.body.style.marginTop = header.offsetHeight + 'px';
 
 // Carousel
-if (carouselItems.length > 0) {
-    let i = 0;
-    carouselItems[0].classList.add('is-active');
-    function toggleActive() {
-        i >= carouselItems.length ? i = 0 : null;
-        i < 0 ? i = carouselItems.length - 1 : null;
-        carouselItems.forEach(item => item.classList.remove('is-active'));
-        carouselItems[i].classList.add('is-active');
+if(carouselItems) {
+    if (carouselItems.length > 0) {
+        let i = 0;
+        carouselItems[0].classList.add('is-active');
+        function toggleActive() {
+            i >= carouselItems.length ? i = 0 : null;
+            i < 0 ? i = carouselItems.length - 1 : null;
+            carouselItems.forEach(item => item.classList.remove('is-active'));
+            carouselItems[i].classList.add('is-active');
+        }
+        carouselButtonNext.addEventListener('click', () => {
+            i++;
+            toggleActive();
+        });
+        carouselButtonBefore.addEventListener('click', () => {
+            i--;
+            toggleActive();
+        });
     }
-    carouselButtonNext.addEventListener('click', () => {
-        i++;
-        toggleActive();
-    });
-    carouselButtonBefore.addEventListener('click', () => {
-        i--;
-        toggleActive();
-    });
 }
 
 navbar.style.top = header.offsetHeight + 'px';
@@ -47,24 +48,27 @@ hamburger.addEventListener('click', () => {
 
 
 // Encuentros Slide Cards
-let i = 0;
-function handleEncuentrosCards() {
-    if (i < -cards.length + 1) {
-        i = 0;
+if (encuentrosCards) {
+    const cards = [...encuentrosCards.children];
+    let i = 0;
+    function handleEncuentrosCards() {
+        if (i < -cards.length + 1) {
+            i = 0;
+        }
+        if (i > 0) {
+            i = -cards.length + 1;
+        }
+        for (const card of cards) {
+            card.style.transform = `translateX(${i * 100}%)`;
+            console.log(i);
+        }
     }
-    if (i > 0) {
-        i = -cards.length + 1;
-    }
-    for (const card of cards) {
-        card.style.transform = `translateX(${i * 100}%)`;
-        console.log(i);
-    }
+    EncuentrosBtnAfter.addEventListener('click', () => {
+        i++;
+        handleEncuentrosCards();
+    });
+    EncuentrosBtnBefore.addEventListener('click', () => {
+        i--;
+        handleEncuentrosCards();
+    });
 }
-EncuentrosBtnAfter.addEventListener('click', () => {
-    i++;
-    handleEncuentrosCards();
-});
-EncuentrosBtnBefore.addEventListener('click', () => {
-    i--;
-    handleEncuentrosCards();
-});
