eeb8e83a925dfac13febc7b81295c76fa13133ff
Aldair Gonzalez <aldairgome97@gmail.com>
2022-08-19 17:23:21 -0500

    Slide de Referencias Responsive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const header = document.getElementById('Header');
 
+// Carousel
 const carouselButtonBefore = document.getElementById('Carousel-button-before');
 const carouselButtonNext = document.getElementById('Carousel-button-next');
 const carouselItems = [...document.getElementsByClassName('Carousel-item')];
@@ -13,32 +14,47 @@ const encuentrosCards = document.getElementById('Encuentros-cards');
 const EncuentrosBtnAfter = document.getElementById('Encuentros-btn--after');
 const EncuentrosBtnBefore = document.getElementById('Encuentros-btn--before');
 
+// Slide
+const ReferencesItem = [...document.getElementsByClassName('References-item')];
+const ReferencesButtonBefore = document.getElementById('References-button-before');
+const ReferencesButtonAfter = document.getElementById('References-button-after');
+const ReferencesCircles = [...document.getElementsByClassName('References-circle')];
+
 
 
 // Margen a todo el body
 document.body.style.marginTop = header.offsetHeight + 'px';
 
+
 // Carousel
-if(carouselItems) {
-    if (carouselItems.length > 0) {
-        let i = 0;
-        carouselItems[0].classList.add('is-active');
-        function toggleActive() {
-            i >= carouselItems.length ? i = 0 : null;
-            i < 0 ? i = carouselItems.length - 1 : null;
-            carouselItems.forEach(item => item.classList.remove('is-active'));
-            carouselItems[i].classList.add('is-active');
+function handleCarousel(array, after, before, circles) {
+    if(array) {
+        if (array.length > 0) {
+            let i = 0;
+            array[0].classList.add('is-active');
+            function toggleActive() {
+                i >= array.length ? i = 0 : null;
+                i < 0 ? i = array.length - 1 : null;
+                array.forEach(item => item.classList.remove('is-active'));
+                array[i].classList.add('is-active');
+                circles ? circles.forEach(item => item.classList.remove('is-active')) : null;
+                circles ? circles[i].classList.add('is-active') : null;
+            }
+            after.addEventListener('click', () => {
+                i++;
+                toggleActive();
+            });
+            before.addEventListener('click', () => {
+                i--;
+                toggleActive();
+            });
         }
-        carouselButtonNext.addEventListener('click', () => {
-            i++;
-            toggleActive();
-        });
-        carouselButtonBefore.addEventListener('click', () => {
-            i--;
-            toggleActive();
-        });
     }
 }
+
+carouselItems ? handleCarousel(carouselItems, carouselButtonNext, carouselButtonBefore) : null;
+ReferencesItem ? handleCarousel(ReferencesItem, ReferencesButtonAfter, ReferencesButtonBefore, ReferencesCircles) : null;
+
 
 navbar.style.top = header.offsetHeight + 'px';
 
