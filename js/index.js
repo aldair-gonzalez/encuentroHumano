8c395571c66f1d1103806d5def1b76eef1b1ffd3
Aldair Gonzalez <aldairgome97@gmail.com>
2022-08-19 11:46:17 -0500

    Seccion Encuentros responsive

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
 const header = document.getElementById('Header');
 
-// Margen a todo el body
-document.body.style.marginTop = header.offsetHeight + 'px';
-
 const carouselButtonBefore = document.getElementById('Carousel-button-before');
 const carouselButtonNext = document.getElementById('Carousel-button-next');
-
 const carouselItems = [...document.getElementsByClassName('Carousel-item')];
-
 
 // Navbar
 const navbar = document.getElementById('Navbar');
 const hamburger = document.getElementById('Hamburger');
+
+// Encuentros Slide Cards
+const encuentrosCards = document.getElementById('Encuentros-cards');
+const cards = [...encuentrosCards.children];
+const EncuentrosBtnAfter = document.getElementById('Encuentros-btn--after');
+const EncuentrosBtnBefore = document.getElementById('Encuentros-btn--before');
+
+
+
+// Margen a todo el body
+document.body.style.marginTop = header.offsetHeight + 'px';
 
 // Carousel
 if (carouselItems.length > 0) {
@@ -37,4 +43,28 @@ navbar.style.top = header.offsetHeight + 'px';
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('is-active');
+});
+
+
+// Encuentros Slide Cards
+let i = 0;
+function handleEncuentrosCards() {
+    if (i < -cards.length + 1) {
+        i = 0;
+    }
+    if (i > 0) {
+        i = -cards.length + 1;
+    }
+    for (const card of cards) {
+        card.style.transform = `translateX(${i * 100}%)`;
+        console.log(i);
+    }
+}
+EncuentrosBtnAfter.addEventListener('click', () => {
+    i++;
+    handleEncuentrosCards();
+});
+EncuentrosBtnBefore.addEventListener('click', () => {
+    i--;
+    handleEncuentrosCards();
 });
